69c309e96dd9624ee07b8506b8470c45714352ff
German <german.o@tcs.com>
2017-06-07 10:05:12 -0500

    Login

--- TicketsApp/TicketsApp/TicketsApp/ViewModels/LoginViewModel.cs
+++ TicketsApp/TicketsApp/TicketsApp/ViewModels/LoginViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TicketsApp.Services;
 
 namespace TicketsApp.ViewModels
@@ -13,6 +15,77 @@ namespace TicketsApp.ViewModels
         #region Attributes
         private ApiService apiService;
         private DialogService dialogService;
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Propperties
+        public string Email
+        {
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+                }
+            }
+            get
+            {
+                return email;
+            }
+        }
+        public string Password
+        {
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Password"));
+                }
+            }
+            get
+            {
+                return password;
+            }
+        }
+        public bool IsRunning
+        {
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+                }
+            }
+            get
+            {
+                return isRunning;
+            }
+        }
+        public bool IsEnabled
+        {
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+            get
+            {
+                return isEnabled;
+            }
+        }
         #endregion
 
         #region Contructors
@@ -20,11 +93,51 @@ namespace TicketsApp.ViewModels
         {
             apiService = new ApiService();
             dialogService = new DialogService();
-        } 
+
+            IsEnabled = true;
+            Email = null;
+            Password = null;
+        }
         #endregion
 
-        #region Events
-        public event PropertyChangedEventHandler PropertyChanged; 
+        #region Commands
+        public ICommand LoginCommand { get { return new RelayCommand(Login); } }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await dialogService.ShowMessage("Error", "You must enter the user email.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await dialogService.ShowMessage("Error", "You must enter a password.");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var response = await apiService.GetUserByEmail("http://checkticketsback.azurewebsites.net/", "/api", "/Users/Login",email,password);
+
+            if (!response.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await dialogService.ShowMessage("Error", "Problem ocurred retrieving user information, try latter.");
+                return;
+            }
+
+            await dialogService.ShowMessage("Tatata", "Tarannnn");
+
+            IsRunning = false;
+            IsEnabled = true;
+
+        }
         #endregion
+
+
     }
 }
